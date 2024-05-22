acbfadbce68e95220bb7f030d912951aeed7ad28
Bill C. <106033471+bi1cor0@users.noreply.github.com>
2024-05-22 16:24:33 -0700

    managed to fix the bug. I had to relaunch the live server a couple of times.

--- src/index.mjs
+++ src/index.mjs
@@ -3,16 +3,16 @@ const usernameVal = document.getElementById("username");
 const emailVal = document.getElementById("email");
 const passVal = document.getElementById("password");
 const checkVal = document.getElementById("passwordCheck");
-console.log(`stuff and stuff`)
-  
-//form.addEventListener("submit", validation);
-//console.log(usernameVal)
-//function validation(e) {
-    //e.preventDefault()
-    //console.log(`string`)
-    //alert(`Name: ${usernameVal}
-    //Email: ${emailVal}
-    //Country: ${passVal}
-    //Password: ${checkVal} lol I'm not actually saving this.`);
- // }
+console.log(`hi`)
+
+form.addEventListener("submit", validation);
+
+function validation(e) {
+    e.preventDefault()
+    console.log(e.target)
+    alert(`Name: ${usernameVal.value}
+    Email: ${emailVal}
+    Country: ${passVal}
+    Password: ${checkVal} lol I'm not actually saving this.`);
+ }
 
