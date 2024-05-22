e32dd891baf0873f5be26e532eb5f0030cefa2bd
Bill C. <106033471+bi1cor0@users.noreply.github.com>
2024-05-22 16:09:02 -0700

    Created validation for the forms example
    There is a bug in this code. I'm not sure, but the Javascript file and the HTML file are not connecting.

---
+++ src/index.mjs
@@ -0,0 +1,18 @@
+const form = document.getElementById("registration");
+const usernameVal = document.getElementById("username");
+const emailVal = document.getElementById("email");
+const passVal = document.getElementById("password");
+const checkVal = document.getElementById("passwordCheck");
+console.log(`stuff and stuff`)
+  
+//form.addEventListener("submit", validation);
+//console.log(usernameVal)
+//function validation(e) {
+    //e.preventDefault()
+    //console.log(`string`)
+    //alert(`Name: ${usernameVal}
+    //Email: ${emailVal}
+    //Country: ${passVal}
+    //Password: ${checkVal} lol I'm not actually saving this.`);
+ // }
+
