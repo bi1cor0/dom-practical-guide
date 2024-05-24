07d0552d633cd81f9710557b953dc59ae5a9d37c
yeahbudd01 <billcorcoro@comcast.net>
2024-05-23 23:42:33 -0700

    Created more functions and tightened up the form interaction.
    Had to relearn HTML collections and how to interact through them in order to complete this portion.

--- src/index.mjs
+++ src/index.mjs
@@ -3,16 +3,27 @@ const usernameVal = document.getElementById("username");
 const emailVal = document.getElementById("email");
 const passVal = document.getElementById("password");
 const checkVal = document.getElementById("passwordCheck");
-console.log(`hi`)
+const radio = document.getElementById("radio-btns")
+const radioBtns = radio.getElementsByTagName("input")
 
 form.addEventListener("submit", validation);
 
 function validation(e) {
     e.preventDefault()
+    const radioResult = radioSelector()
     console.log(e.target)
-    alert(`Name: ${usernameVal.value}
-    Email: ${emailVal}
-    Country: ${passVal}
-    Password: ${checkVal} lol I'm not actually saving this.`);
+    alert(
+    `Name: ${usernameVal.value}
+    Email: ${emailVal.value}
+    Country: ${passVal.value}
+    Password: ${checkVal.value} lol I'm not actually saving this.
+    You said: ${radioResult}`);
+    
  }
 
+ function radioSelector(){
+    for(let i=0; i < radioBtns.length; i++){
+        if(radioBtns[i].type === 'radio' && radioBtns[i].checked) {
+            return radioBtns[i].value
+        }
+    }
